f92fdff169252663d662d5d784e7443d641b16da
Akash Pal <creativeakashpal@gmail.com>
2022-08-01 20:25:41 +0530

    added function to remove task from storage

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -3,17 +3,26 @@ import Task from "./task";
 const task = document.getElementById('task');
 const date = document.getElementById('date');
 
+
+
 function todoData() {
+    let newTask = new Task(task.value, date.value, Math.random().toString(16).slice(2));
     return  {
-        title : task.value,
-        dueDate : date.value,
+        title : newTask.getName(),
+        dueDate : newTask.getDate(),
+        taskId : newTask.getId()
     };  
 }
 
-function addTaskToStorage() {
+export function addTaskToStorage() {
     let taskList = JSON.parse(localStorage.getItem('task')) || [];
     taskList.push(todoData());
     localStorage.setItem('task', JSON.stringify(taskList));
 }
 
-export default addTaskToStorage;
+export function removeTaskFromStorage(index) {
+    let taskList = JSON.parse(localStorage.getItem('task'))
+    taskList.splice(index, 1);
+    localStorage.setItem('task', JSON.stringify(taskList));
+}   
+
