9d3e988cc9d537ef8b87b128f2e398e5efa3089b
Akash Pal <creativeakashpal@gmail.com>
2022-08-01 20:24:22 +0530

    minor changes

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,33 +1,40 @@
 const todo = document.getElementById('todo');
 
-
 function generateTaskUi() 
 {
     todo.innerHTML += `
         <div class="task">
-            <div class="status" data-status></div>
-            <p class="title" data-title></p>
-            <p class="date" data-date></p>
-            <button class="task-delete" data-delete>&#10060;</button>
+            <div class="right">
+                <div class="status" data-status></div>
+                <p class="title" data-title></p>
+            </div>
+            <div class="left">
+                <p class="date" data-date></p>
+                <button class="task-delete" data-delete>&#10060;</button>
+            </div>
         </div>
     `
 
+}
+
+function selectDomElements() {
+    generateTaskUi();
     let title = document.querySelector('#todo > .task:last-child [data-title]');
     let duedate = document.querySelector('#todo > .task:last-child [data-date]');
 
     return {
         title, 
-        duedate
+        duedate,
     };
 }
 
-export function uiAddTask() {
+export function uiShowTask() {
     let taskList = JSON.parse(localStorage.getItem('task'));
     todo.innerHTML = '';
     taskList.forEach(element => {
-        let dom = generateTaskUi();
+        let dom = selectDomElements();
         dom.title.innerText = element.title;
         dom.duedate.textContent = element.dueDate;
+        dom.title.setAttribute('id', element.taskId);
     });
 }
-
